3d21fedf0b04035a984c4adcaea238c8539f946c
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-17 19:31:35 -0500

    migranddo la funcionalidad de contador a un modulo propio

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,12 +1,62 @@
-import { Component } from "@angular/core";
+// import { Component } from "@angular/core";
 
+
+// @Component({
+//   selector: 'app-counter',
+//   template: `<h1> Hola Counter </h1>`,
+
+// })
+// export class CounterComponent {
+//   // templateUrl: './counter.component.html',
+//   // styleUrl: ''
+// }
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: `<h1> Hola Counter </h1>`,
+  template: `
+    <h1> Hola Counter </h1>
+    <h3>Counter: {{counter}} </h3>
+<button type="button" (click)="counterValue(1)" >+</button>
+<button  type="button" (click)="reset()">Reset</button>
+<button  type="button" (click)="counterValue(-1)">-</button>
 
+  `
 })
-export class CounterComponent {
-  // templateUrl: './counter.component.html',
-  // styleUrl: ''
+
+export class CounterComponent  {
+  constructor() { }
+  public counter: number = 0;
+
+
+  counterValue(value:number):void{
+    console.log(value)
+    // if(this.counter >= 0){
+      this.counter += value;
+    // }else if(this.counter === 0 ){
+      // alert("No puedes restar mas.")
+    // }else{
+      // this.counter -= value
+    // }
+  }
+
+  // sum(value:number):void{
+  //   this.counter += value
+  // }
+
+  reset():void{
+    this.counter = 0;
+  }
+
+  // rest(value:number):void{
+  //   if(this.counter === 0){
+  //     alert("No puedes restar mas.")
+
+  //   }else{
+  //     this.counter -= value
+  //   }
+  // }
+
+
 }
