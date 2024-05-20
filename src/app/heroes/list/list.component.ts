8c5afe4acd8cba0c006ab41ac6092fb046188d2b
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-20 15:12:25 -0500

    utilizacion de ng-template y ngIf para mostrar componentes html en la vista

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
   public heroNames: string[] = ['Spiderman', 'Ironman' ,'Hulk', 'Wolverine'];
+  public deletedHero?:string = ''
+
+
+  removeLastHero():void{
+    this.deletedHero = this.heroNames.pop();
+
+    // console.log({deletedHero: deletedHero})
+  }
 
 }
