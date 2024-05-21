645fb5f533ea4f649ef536f1b9776c1b63c9c213
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-20 22:57:46 -0500

    refactorizando la estructura con un modulo propio para heroes e importnado CommonModules en el module de Heores para el correcto funcionamiento por el uso de las directivas ngIf y ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { CounterComponent } from './counter/components/counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+// import { HeroComponent } from './heroes/components/hero/hero.component';
+// import { ListComponent } from './heroes/components/list/list.component';
 import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/components/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     // CounterComponent,
-    HeroComponent,
-    ListComponent
+    // HeroComponent,
+    // ListComponent
   ],
   // lso Module van en los import
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
