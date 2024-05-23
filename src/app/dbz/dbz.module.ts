03740ccda9ee80f941d9f2a2009c179231965cec
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-22 23:50:25 -0500

    refactorizando el codigo para hacerlo modular y segmentar funcionalidades

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+// import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    MainPageComponent
+    MainPageComponent,
   ]
 })
 export class DbzModule { }
