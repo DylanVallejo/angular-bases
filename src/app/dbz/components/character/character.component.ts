7cc106ee321b8dbf3e2de307afc14fdb19278d50
DylanVallejo <dylan1811vallejo@gmail.com>
2024-05-23 00:30:25 -0500

    utilizando el decorador input para enviar datos del padre hacia el hijo

--- src/app/dbz/components/character/character.component.ts
+++ src/app/dbz/components/character/character.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'add-character',
+  selector: 'dbz-add-character',
   templateUrl: './character.component.html',
   styleUrl: './character.component.css'
 })
--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,6 @@
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+
+  @Input()
+  public characterList : Character[] = [
+    {
+      name:'Trunks',
+      power: 10
+    }
+  ]
 
 }
